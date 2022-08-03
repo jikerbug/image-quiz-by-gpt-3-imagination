fbc7cd4e3c70d0d0ab59874c0055203562e2d1f0
jibaek <jibaek@jibaekui-MacBookPro.local>
2022-08-04 00:06:01 +0900

    error message logged

--- functions/getDrawing.js
+++ functions/getDrawing.js
@@ -22,6 +22,7 @@ module.exports = async function getDrawing(text) {
           reject(err);
         } else {
           //여기서 제대로 못받았을 경우 봇이 꺼지지 않도록 오류처리 할 수 있어야함 (100번중에 한번정도 발생?(이거 포함 3번정도 봄) 원인불명... 서버쪽 오류?...)
+          // 지나치게 긴 입력에 대해서 가끔씩 에러가 니오는 것 같기도 하다 => 아니면 입력 몰렸을때 서버쪽에서 터지는 건가?
           /*/Users/jibaek/markinmars/functions/getDrawing.js:26
           base64Data = base64Data.replace(/^data:image\/png;base64,/, "");
           TypeError: Cannot read properties of null (reading 'replace')
